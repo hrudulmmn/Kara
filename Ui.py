d6f6856a56db7767b4127f51e3e410f71e50f557
hrudulmmn <hrudulkrishna06@gmail.com>
2025-12-04 18:44:19 +0530

    Made UI more colorful using QSS

--- Ui.py
+++ Ui.py
@@ -19,18 +19,20 @@ class Window(QMainWindow):
         self.setCentralWidget(main)
         mainlayout = QVBoxLayout(main)
         self.page=0
-        self.zoom=1.5
-        
+        self.zoom=1.2
+
         icpath = self.resource_path("assets\PediFile_logo.png")
         self.setWindowIcon(QIcon(icpath))
         
         #pdf render area
         mainpdf = QWidget()
+        mainpdf.setObjectName("pdf")
         lay = QHBoxLayout(mainpdf)
         self.pdf = QLabel()
+        self.pdf.setVisible(False)
         self.pdf.setScaledContents(True)
         self.pdf.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        lay.setContentsMargins(20,20,20,20)
+        lay.setContentsMargins(50,20,50,20)
         lay.addWidget(self.pdf,alignment=Qt.AlignmentFlag.AlignCenter)
 
         #scroll area for pdf renderig
@@ -41,20 +43,31 @@ class Window(QMainWindow):
         #summary panel
         split = QSplitter(Qt.Orientation.Horizontal)
 
-        self.togbtn = QPushButton('>>')
-        self.togbtn.setCheckable(True)
-        self.togbtn.toggled.connect(self.summary)
-        lay.addWidget(self.togbtn,alignment=Qt.AlignmentFlag.AlignRight)
+        self.togbtn = QPushButton('')
+        self.togbtn.setIcon(QIcon("assets/sum.svg"))
+        self.togbtn.setObjectName("Sumbtn")
+        self.togbtn.setVisible(False)
+        self.togbtn.clicked.connect(self.summary)
+        self.togbtn.setParent(mainpdf)
+        self.togbtn.setFixedSize(32,48)
         
         self.sumFrame = QFrame()
         self.sumFrame.setFrameShape(QFrame.Shape.StyledPanel)
+        self.sumFrame.setObjectName("frame")
+        close = QPushButton("")
+        close.setIcon(QIcon("assets/close.svg"))
+        close.setObjectName("close")
+        close.setFixedSize(20,20)
+        close.clicked.connect(self.close)
         sumlay = QVBoxLayout(self.sumFrame)
+        sumlay.addWidget(close,alignment=Qt.AlignmentFlag.AlignRight)
 
         self.sumArea = QTextEdit()
         self.sumArea.setReadOnly(True)
         font = self.sumArea.font()
         font.setPointSize(10)
         self.sumArea.setFont(font)
+        self.sumArea.setObjectName("Area")
         sumlay.addWidget(self.sumArea)
 
         split.addWidget(self.scrll)
@@ -63,31 +76,50 @@ class Window(QMainWindow):
         self.split = split
 
         #Navbar
-        navbar = QHBoxLayout()
-
-        opnfile = QPushButton("Open")
+        nav = QWidget()
+        navbar = QHBoxLayout(nav)
+        navbar.setContentsMargins(0,0,0,0)
+        navbar.setSpacing(30)
+        nav.setObjectName("navbar")
+        opnfile = QPushButton("")
+        opnfile.setIcon(QIcon("assets/open.svg"))
+        opnfile.setProperty("class","tools")
         opnfile.clicked.connect(self.opnpdf)
         navbar.addWidget(opnfile)
 
-        pageprev = QPushButton("<")
+        pageprev = QPushButton("")
+        pageprev.setIcon(QIcon("assets/prev.svg"))
+        pageprev.setProperty("class","tools")
         navbar.addWidget(pageprev)
         pageprev.clicked.connect(self.toprev)
 
-        pagenxt = QPushButton(">")
+        pagenxt = QPushButton("")
+        pagenxt.setIcon(QIcon("assets/next.svg"))
+        pagenxt.setProperty("class","tools")
         pagenxt.clicked.connect(self.tonxt)
         navbar.addWidget(pagenxt)
 
-        inzoom = QPushButton("+")
+        inzoom = QPushButton("")
+        inzoom.setIcon(QIcon("assets/inzoom.svg"))
+        inzoom.setProperty("class","tools")
         navbar.addWidget(inzoom)
         inzoom.clicked.connect(self.enlarge)
 
-        outzoom = QPushButton("-")
+        outzoom = QPushButton("")
+        outzoom.setIcon(QIcon("assets/outzoom.svg"))
+        outzoom.setProperty("class","tools")
         navbar.addWidget(outzoom)
         outzoom.clicked.connect(self.minimise)
 
+        mode = QPushButton("")
+        mode.setCheckable(True)
+        mode.setIcon(QIcon("assets/mouse.svg"))
+        mode.setProperty("class","tools")
+        navbar.addWidget(mode,Qt.AlignmentFlag.AlignRight)
+
         self.scrll.verticalScrollBar().valueChanged.connect(self.pageScroll)
         navbar.addStretch()
-        mainlayout.addLayout(navbar)
+        mainlayout.addWidget(nav)
         mainlayout.addWidget(self.split)
     
     def opnpdf(self):
@@ -99,8 +131,11 @@ class Window(QMainWindow):
             except Exception as e:
                 QMessageBox.warning(self,"Error","PDF Load Failed!")
             pixmp = render.render(self.doc,self.page,self.zoom)
+            self.pdf.setVisible(True)
+            self.togbtn.setVisible(True)
             self.pdf.setPixmap(pixmp)
             self.pdf.setFixedSize(pixmp.size())
+            self.toggle()
         
     def tonxt(self):
         if not self.doc:
@@ -121,9 +156,10 @@ class Window(QMainWindow):
             bar.setValue(bar.minimum())
             bar.blockSignals(False)
             self.split.setSizes([1,0])
-            self.togbtn.setText(">>")
+            self.togbtn.setIcon(QIcon("assets/sum.svg"))
             self.togbtn.setCheckable(False)
             self.togbtn.setCheckable(True)
+            self.toggle()
     
     def toprev(self):
         if not self.doc:
@@ -144,9 +180,10 @@ class Window(QMainWindow):
         bar.setValue(bar.maximum())
         bar.blockSignals(False)
         self.split.setSizes([1,0])
-        self.togbtn.setText(">>")
+        self.togbtn.setIcon(QIcon("assets/sum.svg"))
         self.togbtn.setCheckable(False)
         self.togbtn.setCheckable(True)
+        self.toggle()
         
 
     def enlarge(self):
@@ -156,6 +193,7 @@ class Window(QMainWindow):
         pixmp = render.render(self.doc,self.page,self.zoom)
         self.pdf.setPixmap(pixmp)
         self.pdf.setFixedSize(pixmp.size())
+        self.toggle()
     
     def minimise(self):
         if not self.doc:
@@ -164,6 +202,7 @@ class Window(QMainWindow):
         pixmp = render.render(self.doc,self.page,self.zoom)
         self.pdf.setPixmap(pixmp)
         self.pdf.setFixedSize(pixmp.size())
+        self.toggle()
 
     def pageScroll(self,value):
         if not self.doc:
@@ -177,34 +216,37 @@ class Window(QMainWindow):
         if value==min:
             self.toprev()
            
-    def summary(self,state):
+    def summary(self):
         if not self.doc:
             return
         curPage = self.doc.load_page(self.page)
         content = curPage.get_text('text')
         self.pageSum = summarise.pagesummarise(self.doc,self.page,content)
         html = markdown.markdown(self.pageSum)
-
-
-        if state==True:
-            self.split.setSizes([600,200])
-            self.togbtn.setText("<<")
-            self.sumArea.setText(html)
-        
-        if state==False:
-            self.split.setSizes([1,0])
-            self.togbtn.setText(">>")
+        self.split.setSizes([600,200])
+        self.sumArea.setText(html)
+    
+    def close(self):
+        if not self.doc:
+            return
+        self.split.setSizes([1,0])
 
             
+    
     def resource_path(self, relative_path):
         base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
         return os.path.join(base_path, relative_path)
+    
+    def toggle(self):
+        if self.pdf.height()==0 or self.pdf.width()==0:
+            return
+        rect=self.pdf.geometry()
+        x = rect.right()+8
+        y = rect.top() + rect.height()//2-self.togbtn.height()//2
+        self.togbtn.move(x,y)
+
+    def resizeEvent(self,event):
+        super().resizeEvent(event)
+        self.toggle()
 
 
-
-app = QApplication(sys.argv)
-window = Window()
-window.setWindowTitle("PediFile")
-window.resize(1000,750)
-window.show()
-sys.exit(app.exec())
---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+from PyQt6.QtWidgets import QApplication
+from Ui import Window
+
+app = QApplication(sys.argv)
+with open("Design.qss","r") as style:
+            app.setStyleSheet(style.read())
+window = Window()
+window.setWindowTitle("PediFile")
+window.resize(1000,750)
+window.show()
+
+sys.exit(app.exec())
